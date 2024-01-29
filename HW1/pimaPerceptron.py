9ba65d1f75e39cb2c751aaf4ba0b24005a19ab1c
Chae Kim <chaekim0820@gmail.com>
2024-01-29 01:02:16 -0500

    finished all parts no idea if they're correct

--- HW1/pimaPerceptron.py
+++ HW1/pimaPerceptron.py
@@ -11,27 +11,55 @@ class Perceptron:
         self.features = ["Pregnancies", "Glucose", "BloodPressure", "SkinThickness", 
                          "Insulin", "BMI", "DiabetesPedigreeFunction", "Age", "Class"]
         self.weights = [random.random()] * len(self.features)
-        self.bias = random.random()
-        self.learningRate = random.random()
+        self.bias = random.random() * 0.1
+        self.learningRate = random.random() * 0.1
 
     def prediction(self, data):
-        predictions = pd.DataFrame(columns=self.features)
+        predictions = []
         for i in (range(len(data))):
-            calculations = []
+            y = 0
             for j in (range(len(self.features))):
                 net = self.weights[j] * (data.iloc[i, j]) + self.bias
-                calculations.append(net)
-            predictions.loc[i] = calculations
+                y += net
+            predictions.append(y)
         return predictions
 
     def threshold(self, data):
-        print("hello from threshold function")
+        results = []
+        for i in (range(len(data))):
+            if (data[i] >= 0):
+                results.append(1)
+            else:
+                results.append(0)
+        return results
+    
+    def updateWeights(self, results, data):
+        newWeights = [0] * len(self.features)
+        for i in range(len(data)):
+            for j in range(len(data.columns)):
+                actual = data.iloc[i, len(self.features)-1]
+                change = self.learningRate * (actual - results[i]) * data.iloc[i, j]
+                newWeights[j] = newWeights[j] + change
+        return newWeights
+    
+    def updateBias(self, results, data):
+        newBias = 0
+        for i in range(len(results)):
+            change = self.learningRate * (data[i] - results[i])
+            newBias = newBias + change
+        return newBias
+    
+    def learningAlgorithm(self, results, data):
+        weights = self.updateWeights(results, data) # outputs are the class labels
+        bias = self.updateBias(results, data["Class"])
+        return weights, bias
 
     def main(self):
         # Running the main method once is one epoch
-        inputs = pd.read_csv('pima-indians-diabetes.csv', names=self.features)
-        predictions = self.prediction(inputs)
-        self.threshold(predictions)
+        data = pd.read_csv('pima-indians-diabetes.csv', names=self.features)
+        predictions = self.prediction(data)
+        outputs = self.threshold(predictions)
+        self.weights, self.bias = self.learningAlgorithm(outputs, data)
 
 if __name__ == "__main__":
     training = Perceptron()
