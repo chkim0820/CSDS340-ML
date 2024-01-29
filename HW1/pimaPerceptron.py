ca6a422da3aaae2412fe0ea75fbb7c49beeaab0e
Chae Kim <chaekim0820@gmail.com>
2024-01-29 11:58:16 -0500

    something is weird; takes too long

--- HW1/pimaPerceptron.py
+++ HW1/pimaPerceptron.py
@@ -5,8 +5,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import random
 
+# Class Perceptron for running the Perceptron algorithm
 class Perceptron:
 
+    # Initializing class variables for Perceptron
     def __init__(self):
         self.features = ["Pregnancies", "Glucose", "BloodPressure", "SkinThickness", 
                          "Insulin", "BMI", "DiabetesPedigreeFunction", "Age", "Class"]
@@ -14,16 +16,17 @@ class Perceptron:
         self.bias = random.random() * 0.1
         self.learningRate = 0.1
 
+    # Initial prediction; z = w^T * w + b
     def prediction(self, data):
-        predictions = []
-        for i in (range(len(data))):
-            y = 0
+        predictions = [] # Prediction for each sample
+        for i in (range(len(data))): # for each sample
+            net = 0
             for j in (range(len(self.features))):
-                net = self.weights[j] * (data.iloc[i, j]) + self.bias
-                y += net
-            predictions.append(y)
+                net += self.weights[j] * (data.iloc[i, j])
+            predictions.append(net + self.bias)
         return predictions
 
+    # For executing the threshold function
     def threshold(self, data):
         results = []
         for i in (range(len(data))):
@@ -33,27 +36,31 @@ class Perceptron:
                 results.append(0)
         return results
     
+    # For updating the weights
     def updateWeights(self, results, data):
         newWeights = [0] * len(self.features)
         for i in range(len(data)):
             for j in range(len(data.columns)):
-                actual = data.iloc[i, len(self.features)-1]
+                actual = data.iloc[i, len(self.features)-1] # actual output for ith sample
                 change = self.learningRate * (actual - results[i]) * data.iloc[i, j]
-                newWeights[j] = newWeights[j] + change
+                newWeights[j] = newWeights[j] + change # update for jth feature
         return newWeights
     
+    # For updating the bias
     def updateBias(self, results, data):
         newBias = 0
-        for i in range(len(results)):
+        for i in range(len(results)): # for each sample
             change = self.learningRate * (data[i] - results[i])
             newBias = newBias + change
         return newBias
     
+    # Learning algorithm; for updating parameters
     def learningAlgorithm(self, results, data):
         weights = self.updateWeights(results, data) # outputs are the class labels
         bias = self.updateBias(results, data["Class"])
         return weights, bias
     
+    # Calculates the rate of correct classification out of all
     def accuracyRate(self, actual, calculated):
         num = 0
         for i in range(len(actual)):
@@ -61,17 +68,18 @@ class Perceptron:
                 num += 1
         return (num / len(actual))
 
+    # Main function; running this method once is one epoch
     def main(self, iter):
         accuracy = 0
+        data = pd.read_csv('pima-indians-diabetes.csv', names=self.features)
         while (accuracy != 1):
-            # Running the main method once is one epoch
-            data = pd.read_csv('pima-indians-diabetes.csv', names=self.features)
-            predictions = self.prediction(data)
+            predictions = self.prediction(data) # contains output to z = wx + b
             outputs = self.threshold(predictions)
             self.weights, self.bias = self.learningAlgorithm(outputs, data)
             accuracy = self.accuracyRate(data["Class"], outputs)
-        print(outputs)
 
+# Main function; for running the Perceptron class
+#Input the desired number of epochs
 if __name__ == "__main__":
     training = Perceptron()
-    training.main(15)
+    training.main(1)
