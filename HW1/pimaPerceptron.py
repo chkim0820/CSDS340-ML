d866add5eb38bdcaf068a21c59060c309f7e54a1
Chae Kim <chaekim0820@gmail.com>
2024-01-30 13:06:46 -0500

    idk it oscilliates?

--- HW1/pimaPerceptron.py
+++ HW1/pimaPerceptron.py
@@ -12,76 +12,84 @@ class Perceptron:
     def __init__(self):
         self.features = ["Pregnancies", "Glucose", "BloodPressure", "SkinThickness", 
                          "Insulin", "BMI", "DiabetesPedigreeFunction", "Age", "Class"]
-        self.weights = [random.random() * 0.1] * len(self.features) 
-        self.bias = random.random() * 0.1
+        self.weights = [0] * (len(self.features) - 1) # Exclude "Class"
+        self.bias = 0
         self.learningRate = 0.1
 
-    # Initial prediction; z = w^T * w + b
+    # Initial prediction; z = w^T * x + b
     def prediction(self, data):
-        predictions = [] # Prediction for each sample
-        # For each sample
+        predictions = [] # Stores the predictions for each sample
+        # For each training sample
         for i in (range(len(data))):
-            net = 0 # The z value; z = w^T * w + b
-            # For each feature
-            for j in (range(len(self.features))):
-                net += self.weights[j] * (data.iloc[i, j])
-            predictions.append(net + self.bias) # add bias at the end of the 
+            net = 0 # The z value; z = w^T * x + b
+            # For each feature; exclude the actual output ("Class")
+            for j in (range(len(self.features) - 1)): # w^T * x
+                net += self.weights[j] * (data.iloc[i, j]) # jth feature
+            net = net + self.bias # Add bias
+            predictions.append(net)
         return predictions
 
     # For executing the threshold function
     def threshold(self, data):
-        results = []
+        results = [] # Store the predicted outputs for each sample
+        # For each training sample
         for i in (range(len(data))):
-            if (data[i] >= 0):
+            if (data[i] >= 0): # Perceptron fires
                 results.append(1)
-            else:
+            else: # Perceptron does not fire
                 results.append(0)
         return results
     
     # For updating the weights
     def updateWeights(self, results, data):
-        newWeights = [0] * len(self.features)
-        for i in range(len(data)):
-            for j in range(len(data.columns)):
-                actual = data.iloc[i, len(self.features)-1] # actual output for ith sample
+        newWeights = self.weights.copy()
+        # For each training sample
+        for i in (range(len(data))):
+            # For each feature; exclude the actual output column
+            for j in (range(len(self.features) - 1)):
+                actual = data.iloc[i, len(self.features) - 1] # actual output for ith sample
                 change = self.learningRate * (actual - results[i]) * data.iloc[i, j]
                 newWeights[j] = newWeights[j] + change # update for jth feature
         return newWeights
     
     # For updating the bias
     def updateBias(self, results, data):
-        newBias = 0
-        for i in range(len(results)): # for each sample
+        newBias = self.bias
+        # For each training sample
+        for i in (range(len(data))): # for each sample
             change = self.learningRate * (data[i] - results[i])
             newBias = newBias + change
         return newBias
     
     # Learning algorithm; for updating parameters
-    def learningAlgorithm(self, results, data):
-        weights = self.updateWeights(results, data) # outputs are the class labels
-        bias = self.updateBias(results, data["Class"])
+    def learningAlgorithm(self, predictions, data):
+        weights = self.updateWeights(predictions, data)
+        bias = self.updateBias(predictions, data["Class"])
         return weights, bias
     
     # Calculates the rate of correct classification out of all
     def accuracyRate(self, actual, calculated):
-        num = 0
+        num = 0 # Stores the number of correct predictions
+        # For each sample
         for i in range(len(actual)):
             if (actual[i] == calculated[i]):
                 num += 1
-        return (num / len(actual))
+        return (num / len(actual)) # ratio of correct predictions out of entire data
 
     # Main function; running this method once is one epoch
-    def main(self, iter):
+    def main(self, iter=None):
         accuracy = 0
         data = pd.read_csv('pima-indians-diabetes.csv', names=self.features)
-        while (accuracy != 1):
+        while (iter >= 0):
             predictions = self.prediction(data) # contains output to z = wx + b
             outputs = self.threshold(predictions)
             self.weights, self.bias = self.learningAlgorithm(outputs, data)
             accuracy = self.accuracyRate(data["Class"], outputs)
+            iter -= 1
+            print(accuracy)
 
 # Main function; for running the Perceptron class
-#Input the desired number of epochs
+# Input the desired number of epochs
 if __name__ == "__main__":
     training = Perceptron()
-    training.main(1)
+    training.main(20) # Specify the number of epochs
