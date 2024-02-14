7d9250067f4842053e8a25a90544956f73d30b0b
Chae Kim <chaekim0820@gmail.com>
2024-02-14 13:41:34 -0500

    set up for number 8

---
+++ HW2/ModelsForWineData.py
@@ -0,0 +1,34 @@
+# Chaehyeon Kim (cxk445); for CSDS 340 Homework Assignment 2
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Class SVM for running the SVM algorithm
+class LogisticRegression:
+
+    # Initializing class variables for SVM
+    def __init__(self):
+        print()
+
+    # Main function; running this method once is one epoch
+    def main(self):
+        print()
+
+# Class SVM for running the SVM algorithm
+class SVM:
+
+    # Initializing class variables for SVM
+    def __init__(self):
+        print()
+
+    # Main function; running this method once is one epoch
+    def main(self):
+        print()
+        
+# Main function; for running the different types of models for the Wine Dataset
+# Input the desired number of epochs
+if __name__ == "__main__":
+    SVMTraining = SVM()
+    LRTraining = LogisticRegression()
+    SVMTraining.main(10) # Specify the number of epochs
