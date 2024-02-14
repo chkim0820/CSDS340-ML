6af44b5b822a413921318df7967a0b41fc518e50
Chae Kim <chaekim0820@gmail.com>
2024-02-14 15:33:09 -0500

    everything works

--- HW2/lr_svm_wine.py
+++ HW2/lr_svm_wine.py
@@ -4,23 +4,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 
-# Class LogisticRegression for running the Logistic Regression algorithm
-class LogisticRegressionProcessor:
-
-    # Main function; running this method once is one epoch
-    def main(self, X_train, X_test, y_train, y_test):
-        LRModel = LogisticRegression() # Creating a logistic regression model
-        LRModel.fit(X_train, y_train) # Training the model
-        LRPred = LRModel.predict(X_test) # Making predictions
-        print("Accuracy:", accuracy_score(y_test, LRPred)) # Calculating accuracy
-        print("Classification Report:\n", classification_report(y_test, LRPred))
-
-# Class SVM for running the SVM algorithm
-class SVMProcessor:
+# Class for running the SVM algorithm
+class SVMClassifier:
 
     # Main function; running this method once is one epoch
     def main(self, X_train, X_test, y_train, y_test):
@@ -29,6 +16,18 @@ class SVMProcessor:
         SVMPred = SVMModel.predict(X_test) # Making predictions
         print("Accuracy:", accuracy_score(y_test, SVMPred)) # Calculating accuracy
         print("Classification Report:\n", classification_report(y_test, SVMPred))
+
+
+# Class for running the Logistic Regression algorithm
+class LogisticRegressionClassifier:
+
+    # Main function; running this method once is one epoch
+    def main(self, X_train, X_test, y_train, y_test):
+        LRModel = LogisticRegression() # Creating a logistic regression model
+        LRModel.fit(X_train, y_train) # Training the model
+        LRPred = LRModel.predict(X_test) # Making predictions
+        print("Accuracy:", accuracy_score(y_test, LRPred)) # Calculating accuracy
+        print("Classification Report:\n", classification_report(y_test, LRPred))
 
 
 # Main function; for running the different types of models for the Wine Dataset
@@ -41,7 +40,7 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = train_test_split(features, classes, test_size=0.5, random_state=1)
 
     # For running the models with the dataset above
-    SVMTraining = SVMProcessor(X_train, X_test, y_train, y_test)
-    LRTraining = LogisticRegressionProcessor(X_train, X_test, y_train, y_test)
-    SVMTraining.main()
-    LRTraining.main() 
+    SVMTraining = SVMClassifier()
+    LRTraining = LogisticRegressionClassifier()
+    SVMTraining.main(X_train, X_test, y_train, y_test)
+    LRTraining.main(X_train, X_test, y_train, y_test) 
